c29f31f2247d1945d49dcc6e1ab3d09aea2afc88
Denis Efimov <defimov9@gmail.com>
2023-10-16 23:22:59 +0500

    feat: настроить css модули

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Counter.scss';
+import cls from './Counter.module.scss';
 
 const Counter = () => {
     const [count, setCount] = useState(0);
@@ -9,7 +9,9 @@ const Counter = () => {
     return (
         <div>
             <h1>{count}</h1>
-            <button onClick={increment}>increment</button>
+            <button className={cls.btn} onClick={increment}>
+                increment
+            </button>
         </div>
     );
 };
