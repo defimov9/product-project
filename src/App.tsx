84ca40b338a57d20580ab9c5460e4fe901963fb5
Denis Efimov <defimov9@gmail.com>
2023-10-17 14:59:59 +0500

    feat: добавить изменение темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { Route, Routes, Link } from 'react-router-dom';
 import { Suspense } from 'react';
 
-import './index.scss';
+import './styles/index.scss';
 
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
+import { useTheme } from './theme/useTheme';
+
 const App = () => {
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <div>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path='/about' element={<AboutPageAsync />} />
-                    <Route path='/' element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPageAsync />} />
+                    <Route path={'/'} element={<MainPageAsync />} />
                 </Routes>
             </Suspense>
         </div>
