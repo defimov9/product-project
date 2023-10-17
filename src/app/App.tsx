767efe9c6045e95ba09ed3d90426839b553d0c33
Denis Efimov <defimov9@gmail.com>
2023-10-17 16:15:05 +0500

    feat: подготовить структуру проекта под FSD

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,11 @@ import { Suspense } from 'react';
 
 import './styles/index.scss';
 
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
@@ -19,8 +19,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>
