7a59c8d145cbcca2e122a04f50444685c8dea26e
Denis Efimov <defimov9@gmail.com>
2023-10-17 14:15:59 +0500

    feat: добавить роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import Counter from './components/Counter';
+import { Route, Routes, Link } from 'react-router-dom';
+import { Suspense } from 'react';
+
 import './index.scss';
+
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 const App = () => {
     return (
         <div>
-            <Counter />
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path='/about' element={<AboutPageAsync />} />
+                    <Route path='/' element={<MainPageAsync />} />
+                </Routes>
+            </Suspense>
         </div>
     );
 };
